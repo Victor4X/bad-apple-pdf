a151ce7564d3209d52a78433fdfca1ab47fb6450
Victor Røer <viggo00@hotmail.com>
2021-04-10 23:36:12 +0200

    Victory.

---
+++ main.py
@@ -0,0 +1,50 @@
+from reportlab.lib.colors import blue
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+import json
+
+canvas = Canvas("BadApple.pdf", pagesize=A4)
+ 
+WIDTH, HEIGHT = A4
+FONT_SIZE = 10
+
+X_OFFSET, Y_OFFSET = 9, HEIGHT-204 
+
+# Set font
+pdfmetrics.registerFont(TTFont("FreeMono","C:\\Users\\viggo\\AppData\\Local\\Microsoft\\Windows\\Fonts\\FreeMono.ttf"))
+canvas.setFont("FreeMono", FONT_SIZE)
+
+# Draw blue text one inch from the left and ten
+# inches from the bottom
+
+
+print(canvas.getAvailableFonts())
+# exit()
+
+canvas.setFillColor(blue)
+with open("data/data.json", "r") as read_file:
+    arr = json.load(read_file)
+
+
+
+for i in range(0,6572): #6572
+    frame = arr[i]
+    for y in range(72):
+        line = ""
+        for x in range(96):
+            if frame[y][x] == 1:
+                line += "■"
+            else:
+                line += " "
+        canvas.drawString(X_OFFSET, Y_OFFSET-y*(FONT_SIZE*0.6), line)
+    canvas.showPage()
+    canvas.setFont("FreeMono", FONT_SIZE)
+    canvas.setFillColor(blue)
+
+
+
+# Save the PDF file
+canvas.save()
